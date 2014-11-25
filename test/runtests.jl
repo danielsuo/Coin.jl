61c7ad0a4d58f98c79227a01776b52faf7b00619
Daniel Suo <danielsuo@gmail.com>
2014-11-25 16:28:04 -0500

    add another SHA256 test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,12 @@
 using Coin
 using Base.Test
 
+##############################################################################
+##
+## Crypto tests
+##
+##############################################################################
+
+# SHA2 tests
 @test Coin.Crypto.SHA2.sha256("a") == "ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb"
+@test Coin.Crypto.SHA2.sha256("Scientific progress goes 'boink'") == "2f2ba2a09a66771bf1fdf541af6e9db4b443145f9935ddd5d4c323c21a8bdcee"
